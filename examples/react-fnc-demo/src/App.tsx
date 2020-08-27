20c21dac5047a01daddba5f761b39a12e3eea075
phongnn <phongnn@gmail.com>
2020-08-27 23:20:13 +0800

    Remove example

--- examples/react-fnc-demo/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Store, createStore } from "@fnc/core";
-import { useQuery, Provider } from "@fnc/react";
-
-function App() {
-  return null;
-}
-
-export default App;
