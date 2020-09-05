debf344e1a30a85d5a237616bd1be2cfc8017162
phongnn <phongnn@gmail.com>
2020-09-05 15:03:41 +0800

    [react-fnc] fetchMore

--- packages/react/src/useQuery.ts
+++ packages/react/src/useQuery.ts
@@ -4,39 +4,103 @@ import { QueryOptions, Query } from "@fnc/core"
 import { useStore } from "./Provider"
 import { useDeepCompareEffect } from "./helpers"
 
-interface DataLoadingState<TData> {
-  loading: boolean
-  data: TData | null
-  error: Error | null
+interface FetchMoreFn<TResult, TArguments, TContext> {
+  (options: {
+    arguments?: TArguments
+    context?: TContext
+    query?: Query<TResult, TArguments, TContext>
+  }): void
 }
 
-type DataLoadingAction<TData> =
-  | { type: "ACTION_REQUEST" }
-  | { type: "ACTION_SUCCESS"; payload: TData }
-  | { type: "ACTION_FAILURE"; payload: Error }
+const defaultFetchMore = () => {
+  throw new Error(
+    `[react-fnc] fetchMore() can only be called after successful data loading.`
+  )
+}
 
-const initialState: DataLoadingState<any> = {
+interface DataLoadingState<TResult, TArguments, TContext> {
+  loading: boolean
+  data: TResult | null
+  error: Error | null
+  fetchMore: FetchMoreFn<TResult, TArguments, TContext>
+  fetchingMore: boolean
+  fetchMoreError: Error | null
+}
+
+type DataLoadingAction<TResult, TArguments, TContext> =
+  | { type: "ACTION_REQUEST" }
+  | {
+      type: "ACTION_SUCCESS"
+      payload: TResult
+      internalFetchMore: (
+        nextQuery: Query<TResult, TArguments, TContext>,
+        callbacks?: {
+          onRequest?: () => void
+          onError?: (err: Error) => void
+        }
+      ) => void
+    }
+  | { type: "ACTION_FAILURE"; payload: Error }
+  | { type: "ACTION_FETCH_MORE_REQUEST" }
+  | { type: "ACTION_FETCH_MORE_ERROR"; payload: Error }
+
+const initialState: DataLoadingState<any, any, any> = {
   loading: false,
   data: null,
   error: null,
+  fetchMore: defaultFetchMore,
+  fetchingMore: false,
+  fetchMoreError: null,
 }
 
 export function useQuery<TResult, TArguments, TContext>(
   query: Query<TResult, TArguments, TContext>,
-  options?: Omit<QueryOptions<TArguments, TContext>, "shape">
+  options?: Omit<QueryOptions<TResult, TArguments, TContext>, "shape">
 ) {
   const store = useStore()
 
-  const [{ data, loading, error }, dispatch] = useReducer<
-    Reducer<DataLoadingState<TResult>, DataLoadingAction<TResult>>
+  const [state, dispatch] = useReducer<
+    Reducer<
+      DataLoadingState<TResult, TArguments, TContext>,
+      DataLoadingAction<TResult, TArguments, TContext>
+    >
   >((state, action) => {
     switch (action.type) {
       case "ACTION_REQUEST":
-        return { loading: true, data: null, error: null }
-      case "ACTION_SUCCESS":
-        return { loading: false, data: action.payload, error: null }
+        return { ...state, loading: true, error: null, data: null }
       case "ACTION_FAILURE":
-        return { loading: false, data: null, error: action.payload }
+        return { ...state, loading: false, data: null, error: action.payload }
+      case "ACTION_SUCCESS":
+        const { payload, internalFetchMore } = action
+        return {
+          ...state,
+          loading: false,
+          data: payload,
+          fetchingMore: false,
+          fetchMore: ({ arguments: args, context, query: anotherQuery }) => {
+            const nextQuery = anotherQuery
+              ? new Query(anotherQuery.name, anotherQuery.fetcher, {
+                  ...anotherQuery.options,
+                  arguments: args || anotherQuery.options.arguments,
+                  // context: { ...anotherQuery.options.context, ...context } // TODO
+                })
+              : new Query(query.name, query.fetcher, {
+                  ...query.options,
+                  arguments: args || query.options.arguments,
+                  // context: { ...anotherQuery.options.context, ...context } // TODO
+                })
+
+            internalFetchMore(nextQuery, {
+              onRequest: () => dispatch({ type: "ACTION_FETCH_MORE_REQUEST" }),
+              onError: (err) =>
+                dispatch({ type: "ACTION_FETCH_MORE_ERROR", payload: err }),
+            })
+          },
+        }
+      case "ACTION_FETCH_MORE_REQUEST":
+        return { ...state, fetchingMore: true, fetchMoreError: null }
+      case "ACTION_FETCH_MORE_ERROR":
+        return { ...state, fetchingMore: false, fetchMoreError: action.payload }
       default:
         return state
     }
@@ -46,19 +110,21 @@ export function useQuery<TResult, TArguments, TContext>(
     const { name, fetcher, options: defaultOpts } = query
     const actualQuery = !options
       ? query
-      : new Query(name, fetcher, { ...defaultOpts, ...options })
+      : new Query(name, fetcher, { ...defaultOpts, ...options }) // TODO: merge context objects
 
-    const subscription = store.registerQuery<TResult, TArguments, TContext>(
-      actualQuery,
-      {
-        onRequest: () => dispatch({ type: "ACTION_REQUEST" }),
-        onData: (data) => dispatch({ type: "ACTION_SUCCESS", payload: data }),
-        onError: (err) => dispatch({ type: "ACTION_FAILURE", payload: err }),
-      }
-    )
+    const { fetchMore: internalFetchMore, unregister } = store.registerQuery<
+      TResult,
+      TArguments,
+      TContext
+    >(actualQuery, {
+      onRequest: () => dispatch({ type: "ACTION_REQUEST" }),
+      onData: (data) =>
+        dispatch({ type: "ACTION_SUCCESS", payload: data, internalFetchMore }),
+      onError: (err) => dispatch({ type: "ACTION_FAILURE", payload: err }),
+    })
 
-    return () => subscription.unsubscribe()
+    return () => unregister()
   }, [store, query, options])
 
-  return { loading, data, error }
+  return state
 }
