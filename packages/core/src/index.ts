2d14e11485880203504c6800b61d01cc6defd1de
phongnn <phongnn@gmail.com>
2020-09-09 11:12:28 +0800

    [react-fnc] Implement useMutation hook

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
 export { Query, QueryOptions } from "./query/Query"
+export { Mutation, MutationOptions } from "./query/Mutation"
 export { Store, InternalStore, StoreOptions } from "./store/Store"
 export { createStore } from "./store/createStore"
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,2 +1,3 @@
 export { Provider } from "./Provider"
 export { useQuery } from "./useQuery"
+export { useMutation } from "./useMutation"
