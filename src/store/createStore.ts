e8c4164fee0d619b5ab7325707e6c2162b01ba37
Phong Nguyen <phongnn@Phongs-MacBook-Pro.local>
2020-10-21 11:39:06 +0800

    Small updates to StoreOptions

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -1,10 +1,11 @@
-import { Store } from "../common"
+import { Store, StoreOptions } from "../common"
 import AdvancedStoreImpl from "./standard/AdvancedStoreImpl"
 
-export function createStore(options: { queryExpiryMs?: number } = {}): Store {
+export function createStore(options?: StoreOptions): Store {
   const {
     queryExpiryMs = 15 * 60 * 1000, // 15 minutes
-  } = options
+    ...rest
+  } = options || {}
 
-  return new AdvancedStoreImpl({ queryExpiryMs })
+  return new AdvancedStoreImpl({ queryExpiryMs, ...rest })
 }
